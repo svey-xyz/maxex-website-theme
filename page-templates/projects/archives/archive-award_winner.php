851898c5656adae87a9ad76c02b9af36fbbfee73
soulelyhayden <haydensoule@outlook.com>
2021-04-19 23:18:37 -0400

    Fix awards

--- page-templates/projects/archives/archive-award_winner.php
+++ page-templates/projects/archives/archive-award_winner.php
@@ -2,7 +2,6 @@
 	get_header();
 	get_template_part('template-parts/headers/site-header');
 
-	include( locate_template('template-parts/functions/get-project-year.php', false, false ));
 	$theme = get_term_by('slug', get_query_var('theme'), 'general_submission_theme');
 
 	$theme_loaded = !empty($theme) ? true : false;
@@ -16,31 +15,9 @@
 			<h2><?php echo $title ?></h2>
 		</div>
 
-		<div class="project-years">
-			<?php
-			foreach ($years as $year):
-				global $wp;
-				$current_url = home_url( add_query_arg( array(), $wp->request ) );
-				$query_url = add_query_arg('project-year', $year->name, $current_url);
-				$class = '';
-				if ($year_term->name == $year->name):
-					$class = 'active-year';
-				endif;
-			?>
-				<a href="<?php echo $query_url ?>"><span class=<?php echo $class ?>><?php echo $year->name; ?></span></a>
-			<?php endforeach;?>
-		</div>
-
 		<div class="awards-grid block">
 			<?php
 				$award_winner_query = get_posts(array(
-					'tax_query' => array(
-						array(
-							'taxonomy' => 'project_year',
-							'field' => 'name',
-							'terms' => $year_term,
-						),
-					),
 					'post_type' => 'award_winner',
 					'post_status' => 'publish',
 					'posts_per_page' => -1
