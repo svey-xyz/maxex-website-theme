dbce71cd988cf31ef07dae03b354d47e339f9671
soulelyhayden <haydensoule@outlook.com>
2021-04-30 12:57:58 -0400

    Award Winners patch

--- template-parts/cards/award-winner-card.php
+++ template-parts/cards/award-winner-card.php
@@ -10,14 +10,14 @@
 	$project_title = $winning_project->post_title;
 
 	if ($winning_project->post_type == 'general_submission') {
-		$sub = get_field('submission', $winning_project);
-		$image = $sub['image'];
+		// $sub = get_field('submission', $winning_project);
+		$image = get_field('thumbnail', $winning_project);
 
 		$theme_name = get_term(get_field('submission_theme', $winning_project))->slug;
-		$link = "/general-submissions/?theme=" . $theme_name;
+		$link = "/themed-exhibitions/?theme=" . $theme_name;
 
-		$project_title = $sub['image_title'];
-		$student_name = $sub['student_name'];
+		$project_title = get_field('submission_title', $winning_project);
+		$student_name = get_field('student_name', $winning_project);
 	}
 ?>
 
